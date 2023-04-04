f82db8c92049d6a43391e6a2d1bae9dc4526a70b
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2023-04-04 23:19:31 +0530

    added new files

---
+++ backend/data/models/ChatModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const chatModel = mongoose.schema(
+  {
+    chatName: { type: String, trim: true }, //chat is of type string and trim always true as to remove extra space
+    isgroupchat: { type: Boolean, default: false }, // groupchat is not always so it will be either true or false
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId, // it is pf type array cuz there can be many users
+        ref: "User",
+      },
+    ],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      //for current mesage
+      ref: "Message", //referring to the message in the database
+    },
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId, // this will contain  id to the  particular user for reference
+      ref: "User",
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+const Chat = mongoose.model("Chat", chatModel);
+module.exports = Chat;
---
+++ server.js
@@ -0,0 +1,134 @@
+const { json } = require("express");
+const express = require("express"); // to import express
+const connectDB = require("./backend/Config/db");
+const dotenv = require("dotenv");
+
+const chats = [
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "John Doe",
+        email: "john@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "John Doe",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c25468b27c4dd4",
+    chatName: "Guest User",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Anthony",
+        email: "anthony@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c2d468bc7c4dd4",
+    chatName: "Anthony",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "John Doe",
+        email: "jon@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+    ],
+    _id: "617a518c4081150716472c78",
+    chatName: "Friends",
+    groupAdmin: {
+      name: "Guest User",
+      email: "guest@example.com",
+    },
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Jane Doe",
+        email: "jane@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7cfdd4",
+    chatName: "Jane Doe",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "John Doe",
+        email: "jon@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+    ],
+    _id: "617a518c4081150016472c78",
+    chatName: "Chill Zone",
+    groupAdmin: {
+      name: "Guest User",
+      email: "guest@example.com",
+    },
+  },
+];
+
+const app = express(); //create instance of express variable
+dotenv.config();
+// to get the API
+connectDB();
+app.get("/", (req, res) => {
+  res.send("API is running successfully");
+});
+
+app.get("/api/chat", (req, res) => {
+  console.log(chats, "Chats", JSON.stringify(chats), "json");
+  res.send(chats);
+});
+
+app.get("/app/chat/:id", (req, res) => {
+  console.log(req);
+});
+
+// const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`server started on PORT 5000`)); //to start own server on the port
